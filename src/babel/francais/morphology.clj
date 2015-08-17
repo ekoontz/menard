3ca73ef0e8f13e67d521807346db60f5f526a1c3
Eugene Koontz <ekoontz@hiro-tan.org>
2015-08-16 19:21:54 -0700

    -er verbs includes also -ér verbs

--- src/babel/francais/morphology.clj
+++ src/babel/francais/morphology.clj
@@ -134,10 +134,10 @@
                  ar-type (try (re-find #"ar$" infinitive)
                               (catch Exception e
                                 (throw (Exception. (str "Can't regex-find on non-string: " infinitive " from word: " word)))))
-                 er-type (re-find #"er$" infinitive)
+                 er-type (re-find #"[eé]r$" infinitive)
                  ir-type (re-find #"ir$" infinitive)
                  re-type (re-find #"re$" infinitive)
-                 stem (string/replace infinitive #"[iae]r$" "")
+                 stem (string/replace infinitive #"[iaeé]r$" "")
                  stem (if re-type
                         (string/replace infinitive #"re$" "")
                         stem)

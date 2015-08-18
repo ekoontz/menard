335c90250e835a1af1d38b32a00b466eabd8e97b
Eugene Koontz <ekoontz@hiro-tan.org>
2015-08-17 17:18:46 -0700

    add Travis CI-related setup scripts

---
+++ src/sql/babel.sql
@@ -0,0 +1,18 @@
+DROP TABLE expression; 
+
+CREATE TABLE expression (id INTEGER, PRIMARY KEY(id), 
+       created TIMESTAMP DEFAULT now(), 
+       language TEXT, model TEXT,
+       surface TEXT, structure JSONB,
+       serialized TEXT);
+
+CREATE SEQUENCE expression_id_seq
+                     START WITH 1
+                     INCREMENT BY 1
+                     NO MINVALUE
+                     NO MAXVALUE
+                     CACHE 1;
+
+ALTER TABLE ONLY expression ALTER COLUMN id SET DEFAULT nextval('expression_id_seq'::regclass);
+
+

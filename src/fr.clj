4614fcb659ab5e316ab4315efad88a8ffb483770
Eugene Koontz <ekoontz@hiro-tan.org>
2015-09-02 00:38:45 -0700

    promote repair-related code closer to top of tree

--- src/fr.clj
+++ src/fr.clj
@@ -1,38 +1,45 @@
-;; Command line usage: 'lein run -m repair.es.todos/fix'
-(ns repair.es.todos)
+;; Command-line usage:
+;;
+;; lein run -m fr/tout
+;; lein run -m fr/tout <num>
+
+(ns fr)
+
 (require '[babel.writer :refer [fill-by-spec fill-verb write-lexicon]])
 (require '[babel.english :as en])
-(require '[babel.espanol :as es :refer [verbs]])
-(require '[babel.espanol.morphology :as morph :refer [fo]])
-(require '[babel.parse :as parse])
-(require '[babel.reader :as reader])
+(require '[babel.francais :as fr :refer [verbs]])
+(require '[babel.korma :refer :all])
 (require '[babel.repair :refer [process]])
-(require '[clojure.data.json :as json])
-(require '[dag-unify.core :refer [unify deserialize]])
+(require '[clojure.tools.logging :as log])
+(require '[dag-unify.core :refer [unify]])
+(require '[korma.core :refer :all])
+(require '[korma.db :refer :all])
 
-(defn fix [ & [count]]
-  (let [count 10]
-    (write-lexicon "es" @es/lexicon)
+(defn tout [ & [count]]
+  (let [count (if count (Integer. count) 10)]
+    (write-lexicon "fr" @fr/lexicon)
     (process
      (reduce concat
              (map (fn [verb]
                     (map (fn [tense]
                            {:fill
-                            {:spec (unify {:root {:espanol {:espanol verb}}}
+                            {:spec (unify {:root {:français {:français verb}}}
                                           tense)
                              :source-model en/small
-                             :target-model es/small
+                             :target-model fr/small
                              :count count}})
                          (shuffle
                           [
-                           {:synsem {:sem {:aspect :perfect
-                                           :tense :past}}}
                            {:synsem {:sem {:tense :conditional}}}
                            {:synsem {:sem {:tense :futuro}}}
                            {:synsem {:sem {:tense :present}}}
+
+                           ;; TODO: enable this when ready:
+                           ;; {:synsem {:sem {:aspect :perfect
+                           ;;                 :tense :past}}}
+
                            ])
                          ))
                   (sort verbs)
                   )))))
-
 

05d16621ba04907499b578f2864b61e92a51be5c
Eugene Koontz <ekoontz@hiro-tan.org>
2015-09-05 16:28:20 -0700

    same API as fr/ and it/

--- src/es.clj
+++ src/es.clj
@@ -3,40 +3,55 @@
 ;; lein run -m es/todos
 ;; lein run -m es/todos <num>
 
-(ns es)
+(ns es (:refer-clojure :exclude [get-in]))
 
-(require '[babel.writer :refer [fill-by-spec fill-verb write-lexicon]])
+(require '[babel.writer :refer [fill-by-spec fill-verb process write-lexicon]])
 (require '[babel.english :as en])
-(require '[babel.espanol :as es :refer [verbs]])
+(require '[babel.espanol :as es :refer [lexicon]])
 (require '[babel.korma :refer :all])
-(require '[babel.repair :refer [process]])
 (require '[clojure.tools.logging :as log])
-(require '[dag-unify.core :refer [unify]])
+(require '[dag-unify.core :refer [get-in unify]])
 (require '[korma.core :refer :all])
 (require '[korma.db :refer :all])
 
 (defn todos [ & [count]]
   (let [count (if count (Integer. count) 10)]
     (write-lexicon "es" @es/lexicon)
-    (process
-     (reduce concat
-             (map (fn [verb]
-                    (map (fn [tense]
-                           {:fill
-                            {:spec (unify {:root {:espanol {:espanol verb}}}
-                                          tense)
-                             :source-model en/small
-                             :target-model es/small
-                             :count count}})
-                         (shuffle
-                          [
-                           {:synsem {:sem {:aspect :perfect
+    (let [
+          ;; subset of the lexicon: only verbs which are infinitives and that can be roots:
+          ;; (i.e. those that have a specific (non- :top) value for [:synsem :sem :pred])
+          root-verbs 
+          (zipmap
+           (keys @lexicon)
+           (map (fn [lexeme-set]
+                  (filter (fn [lexeme]
+                            (and
+                             (= (get-in lexeme [:synsem :cat]) :verb)
+                             (= (get-in lexeme [:synsem :infl]) :top)
+                             (not (= :top (get-in lexeme [:synsem :sem :pred] :top)))))
+                          lexeme-set))
+                (vals @lexicon)))]
+      (.size (map (fn [verb]
+                    (let [root-form (get-in verb [:espanol :espanol])]
+                      (log/debug (str "generating from root-form:" root-form))
+                      (.size (map (fn [tense]
+                                    (let [spec (unify {:root {:espanol {:espanol root-form}}}
+                                                      tense)]
+                                      (log/debug (str "generating from: " spec))
+                                      (process [{:fill
+                                                 {:spec spec
+                                                  :source-model en/small
+                                                  :target-model es/small}
+                                                 :count count}])))
+                                  [{:synsem {:sem {:tense :conditional}}}
+                                   {:synsem {:sem {:tense :futuro}}}
+                                   {:synsem {:sem {:tense :present}}}
+                                   {:synsem {:sem {:aspect :perfect
                                            :tense :past}}}
-                           {:synsem {:sem {:tense :conditional}}}
-                           {:synsem {:sem {:tense :futuro}}}
-                           {:synsem {:sem {:tense :present}}}
-                           ])
-                         ))
-                  (sort verbs)
-                  )))))
+                                   ]
+                                  ))))
+                  (reduce concat
+                          (map (fn [key]
+                                 (get root-verbs key))
+                               (sort (keys root-verbs)))))))))
 

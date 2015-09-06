3d51825272b1338f6259c4d58f58fdc618776153
franksfo <franksfo2003@gmail.com>
2015-09-06 16:26:53 -0700

    changed all the tense names to english

--- babel/espanol/grammar.clj
+++ babel/espanol/grammar.clj
@@ -195,17 +195,17 @@
                            {:rule "s-future-phrasal"
                             :head {:phrasal true}
                             :synsem {:aux false
-                                     :infl :futuro
+                                     :infl :future
                                      :cat :verb
-                                     :sem {:tense :futuro}}})
+                                     :sem {:tense :future}}})
                    (unifyc c10
                            root-is-head
                            {:rule "s-future-nonphrasal"
                             :head {:phrasal false}
                             :synsem {:aux false
-                                     :infl :futuro
+                                     :infl :future
                                      :cat :verb
-                                     :sem {:tense :futuro}}})
+                                     :sem {:tense :future}}})
                    (unifyc c10
                            root-is-head-root
                            {:rule "s-conditional-phrasal"
@@ -242,19 +242,19 @@
                                            :tense :past}}})
                    (unifyc c10
                            root-is-head-root
-                           {:rule "s-imperfetto-phrasal"
+                           {:rule "s-imperfect-phrasal"
                             :head {:phrasal true}
                             :synsem {:aux false
-                                     :infl :imperfetto
+                                     :infl :imperfect
                                      :cat :verb
                                      :sem {:aspect :progressive
                                            :tense :past}}})
                    (unifyc c10
                            root-is-head
-                           {:rule "s-imperfetto-nonphrasal"
+                           {:rule "s-imperfect-nonphrasal"
                             :head {:phrasal false}
                             :synsem {:aux false
-                                     :infl :imperfetto
+                                     :infl :imperfect
                                      :cat :verb
                                      :sem {:aspect :progressive
                                            :tense :past}}})
@@ -305,12 +305,12 @@
                    (unifyc h21
                            {:rule "vp-future"
                             :synsem {:aux false
-                                     :infl :futuro
+                                     :infl :future
                                      :cat :verb}})
                    (unifyc h21
-                          {:rule "vp-imperfetto"
+                          {:rule "vp-imperfect"
                            :synsem {:aux false
-                                    :infl :imperfetto
+                                    :infl :imperfect
                                     :cat :verb}})
                    (unifyc h21
                            {:rule "vp-past"

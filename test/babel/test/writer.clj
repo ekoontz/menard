1d7badacca10e4d4bb751105500f1abe79500393
Eugene Koontz <ekoontz@hiro-tan.org>
2015-09-06 10:56:21 -0700

    add stub test

---
+++ test/babel/test/writer.clj
@@ -0,0 +1,11 @@
+(ns babel.test.writer
+  (:refer-clojure :exclude [test])
+  (:require [babel.writer :as writer]
+            [clojure.string :as string]
+            [clojure.test :refer :all]
+            [clojure.tools.logging :as log]))
+
+(deftest simple
+  (is (= true true)))
+
+

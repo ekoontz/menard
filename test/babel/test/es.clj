d8cce9c672a468e8ce62e2e182e6f89ba1dc1384
Eugene Koontz <ekoontz@hiro-tan.org>
2015-09-06 11:25:19 -0700

    add a few tests to start

---
+++ test/babel/test/es.clj
@@ -0,0 +1,28 @@
+(ns babel.test.es
+  (:refer-clojure :exclude [get-in])
+  (:require [babel.engine :as engine]
+            [babel.english :as en]
+            [babel.espanol :as es]
+            [babel.espanol.morphology :refer [fo]]
+            [babel.writer :as writer]
+            [clojure.string :as string]
+            [clojure.test :refer :all]
+            [clojure.tools.logging :as log]
+            [dag-unify.core :refer [get-in]]))
+
+
+(deftest generate-conditional
+  (let [result (engine/generate {:synsem {:subcat '()
+                                          :sem {:pred :dormire
+                                                :subj {:pred :io}
+                                                :tense :conditional}}}
+                                es/small
+                                :enrich true)]
+    (is (= :1st (get-in result [:comp :synsem :agr :person])))
+    (is (= :sing (get-in result [:comp :synsem :agr :number])))
+    (is (= "yo dormiría" (fo result)))))
+
+
+
+
+

bb9d3c3250e2a94b7883ed18bec91497ebd66f12
Eugene Koontz <ekoontz@hiro-tan.org>
2015-09-06 16:11:36 -0700

    use English names for keywords

--- test/babel/test/fr.clj
+++ test/babel/test/fr.clj
@@ -1,7 +1,7 @@
 (ns babel.test.fr
   (:refer-clojure :exclude [get-in])
   (:require [babel.engine :as engine]
-            [babel.francais :as fr]
+            [babel.francais :as fr :refer [lexicon]]
             [babel.francais.morphology :refer [fo]]
             [babel.writer :as writer]
             [clojure.string :as string]
@@ -9,17 +9,32 @@
             [clojure.tools.logging :as log]
             [dag-unify.core :refer [get-in]]))
 
-
-(deftest generate-conditional
+(deftest conditional
   (let [result (engine/generate {:synsem {:subcat '()
                                           :sem {:pred :sleep
-                                                :subj {:pred :io}
+                                                :subj {:pred :I}
                                                 :tense :conditional}}}
                                 fr/small
                                 :enrich true)]
     (is (= :1st (get-in result [:comp :synsem :agr :person])))
     (is (= :sing (get-in result [:comp :synsem :agr :number])))
     (is (= "je dormirais" (fo result)))))
+
+(deftest present-irregular
+  (let [result (engine/generate {:synsem {:subcat '()
+                                          :sem {:pred :be
+                                                :subj {:pred :I}
+                                                :tense :present}}}
+                                fr/small)]
+    (is (= "je suis" (fo result)))))
+
+(deftest imperfect-irregular
+  (let [result (engine/generate {:synsem {:subcat '()
+                                          :sem {:pred :be
+                                                :subj {:pred :I}
+                                                :tense :imperfect}}}
+                                fr/small)]
+    (is (= "j'étais" (fo result)))))
 
 
 

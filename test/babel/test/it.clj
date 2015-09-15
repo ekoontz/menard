0e8116494a3f4b4af8dbebbad11846e77b908684
Eugene Koontz <ekoontz@hiro-tan.org>
2015-09-14 20:44:44 -0700

    fix tests: get grammar and lexicon from the right (new) places

--- test/babel/test/it.clj
+++ test/babel/test/it.clj
@@ -1,44 +1,39 @@
 (ns babel.test.it
   (:refer-clojure :exclude [get-in])
-  (:require [babel.engine :as engine]
-            [babel.italiano.writer :as it :refer [lexicon]]
-            [babel.italiano.lexicon :refer [lexicon-source]]
+  (:require [babel.engine :refer [generate]]
+            [babel.italiano.grammar :refer [small]]
+            [babel.italiano.lexicon :refer [lexicon]]
             [babel.italiano.morphology :refer [fo]]
-            [babel.writer :as writer]
-            [clojure.string :as string]
             [clojure.test :refer :all]
             [clojure.tools.logging :as log]
             [dag-unify.core :refer [get-in]]))
 
 (deftest present-irregular
-  (let [result (engine/generate {:synsem {:subcat '()
-                                          :sem {:pred :be
-                                                :subj {:pred :I}
-                                                :tense :present}}}
-                                it/small)]
+  (let [result (generate {:synsem {:subcat '()
+                                   :sem {:pred :be
+                                         :subj {:pred :I}
+                                         :tense :present}}}
+                         small)]
     (is (= "io sono" (fo result)))))
 
 (deftest passato-prossimo
-  (let [result (engine/generate {:root {:italiano {:italiano "bere"}}
-                                 :synsem {:subcat ()
-                                          :sem {:subj {:pred :I}
-                                                :tense :past
-                                                :aspect :perfect}}}
-                                it/small)]
+  (let [result (generate {:root {:italiano {:italiano "bere"}}
+                          :synsem {:subcat ()
+                                   :sem {:subj {:pred :I}
+                                         :tense :past
+                                         :aspect :perfect}}}
+                         small)]
     (and (is (not (nil? result)))
          (is (= "io ho bevuto" (fo result))))))
 
 (deftest passato-prossimo-reflexive
-  (let [result (engine/generate {:head {:synsem {:agr {:gender :fem}}}
-                                 :synsem {:subcat '()
-                                          :infl :present
-                                          :sem {:pred :get-up
-                                                :subj {:pred :I}
-                                                :tense :past
-                                                :aspect :perfect}}}
-                                it/small)]
+  (let [result (generate {:head {:synsem {:agr {:gender :fem}}}
+                          :synsem {:subcat '()
+                                   :infl :present
+                                   :sem {:pred :get-up
+                                         :subj {:pred :I}
+                                         :tense :past
+                                         :aspect :perfect}}}
+                         small)]
     (and (is (not (nil? result)))
          (is (= "io mi sono alzata" (fo result))))))
-
-
-

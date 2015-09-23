1d75cd79a5c8e895c025099115cef64bf27b2c8e
Eugene Koontz <ekoontz@hiro-tan.org>
2015-09-23 07:41:11 -0700

    add correction function for 'camminare'

--- src/babel/italiano/writer.clj
+++ src/babel/italiano/writer.clj
@@ -8,6 +8,51 @@
 (require '[clojure.tools.logging :as log])
 (require '[dag-unify.core :refer (fail? get-in strip-refs unify)])
 
+(defn camminare [ & [count]]
+  (let [count (if count (Integer. count) 10)
+        ;; subset of the lexicon: only verbs which are infinitives and that can be roots:
+        ;; (i.e. those that have a specific (non- :top) value for [:synsem :sem :pred])
+        root-verbs 
+        (zipmap
+         (keys @lexicon)
+         (map (fn [lexeme-set]
+                (filter (fn [lexeme]
+                          (and
+                           (= (get-in lexeme [:synsem :cat]) :verb)
+                           (= (get-in lexeme [:synsem :infl]) :top)
+                           (not (= :top (get-in lexeme [:synsem :sem :pred] :top)))))
+                        lexeme-set))
+              (vals @lexicon)))]
+
+    (delete-from-expressions "it" {:root {:italiano {:italiano "camminare"}}})
+
+    (write-lexicon "it" @lexicon)
+    (log/info (str "done writing lexicon."))
+    (.size (map (fn [verb]
+                  (let [root-form (get-in verb [:italiano :italiano])]
+                    (log/debug (str "generating from root-form:" root-form))
+                    (.size (map (fn [tense]
+                                  (let [spec (unify {:root {:italiano {:italiano root-form}}}
+                                                    tense)]
+                                    (log/debug (str "generating from: " spec))
+                                    (process [{:fill
+                                               {:spec spec
+                                                :source-model en/small-plus-vp-pronoun
+                                                :target-model small
+                                                :count count}}] "it")))
+                                (list {:synsem {:sem {:tense :conditional}}}
+                                      {:synsem {:sem {:tense :future}}}
+                                      {:synsem {:sem {:tense :present}}}
+                                      {:synsem {:sem {:aspect :progressive
+                                                      :tense :past}}}
+                                      {:synsem {:sem {:aspect :perfect
+                                                      :tense :past}}})))))
+                (reduce concat
+                        (map (fn [key]
+                               (get root-verbs key))
+                             (filter #(= % "camminare")
+                                     (sort (keys root-verbs)))))))))
+
 (defn tutti [ & [count]]
   (let [count (if count (Integer. count) 10)
         ;; subset of the lexicon: only verbs which are infinitives and that can be roots:

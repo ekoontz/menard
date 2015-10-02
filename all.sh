987ac459adb784f0ede3be195126ed194705a62a
Eugene Koontz <ekoontz@hiro-tan.org>
2015-10-02 00:43:18 -0700

    generalize use of english.writer/translate

--- all.sh
+++ all.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 lein run -m babel.espanol.writer/todos
+lein run -m babel.english.writer/translate "es"
+
 lein run -m babel.francais.writer/tout
+lein run -m babel.english.writer/translate "fr"
+
 lein run -m babel.italiano.writer/tutti
 lein run -m babel.english.writer/translate "it"
+
+
+

6c42d359e1235982a860e8b41ca8712c538c813f
Eugene Koontz <ekoontz@hiro-tan.org>
2015-10-02 07:56:45 -0700

    write espanol/todos to be like italiano/tutti

--- src/babel/espanol/writer.clj
+++ src/babel/espanol/writer.clj
@@ -102,27 +102,76 @@
                              (= (get-in lexeme [:synsem :infl]) :top)
                              (not (= :top (get-in lexeme [:synsem :sem :pred] :top)))))
                           lexeme-set))
-                (vals @lexicon)))]
-      (.size (map (fn [verb]
-                    (let [root-form (get-in verb [:espanol :espanol])]
-                      (log/info (str "generating from root-form:" root-form))
-                      (.size (map (fn [tense]
-                                    (let [spec (unify {:root {:espanol {:espanol root-form}}}
-                                                      tense)]
-                                      (log/debug (str "generating from: " spec))
-                                      (process [{:fill
-                                                 {:spec spec
-                                                  :source-model en/small
-                                                  :target-model small}
-                                                 :count count}] "es")))
-                                  [{:synsem {:sem {:tense :conditional}}}
-                                   {:synsem {:sem {:tense :future}}}
-                                   {:synsem {:sem {:tense :present}}}
-                                   {:synsem {:sem {:aspect :perfect
-                                           :tense :past}}}
-                                   ]
-                                  ))))
-                  (reduce concat
-                          (map (fn [key]
-                                 (get root-verbs key))
-                               (sort (keys root-verbs)))))))))
+                (vals @lexicon)))
+
+          todos
+          (reduce concat
+                  (map (fn [key]
+                         (get root-verbs key))
+                       (sort (keys root-verbs))))]
+    (write-lexicon "es" @lexicon)
+    (log/info (str "done writing lexicon."))
+    (log/info (str "generating examples with this many verbs:"
+                   (.size todos)))
+
+    (.size (pmap (fn [verb]
+                   (log/trace (str "verb: " (strip-refs verb)))
+                   (let [root-form (get-in verb [:espanol :espanol])]
+                     (log/info (str "generating with verb: '" root-form "'"))
+                     (.size (map (fn [tense]
+                                   (let [spec (unify {:root {:espanol {:espanol root-form}}}
+                                                     tense)]
+                                     (.size
+                                      (map (fn [gender]
+                                             (let [spec (unify spec
+                                                               {:comp {:synsem {:agr gender}}})]
+                                               (log/trace (str "generating from gender: " gender))
+                                               (.size
+                                                (map (fn [person]
+                                                       (let [spec (unify spec
+                                                                         {:comp {:synsem {:agr {:person person}}}})]
+                                                         (log/trace (str "generating from person: " person))
+                                                         (.size
+                                                          (map (fn [number]
+                                                                 (let [spec (unify spec
+                                                                                   {:comp {:synsem {:agr {:number number}}}})]
+                                                                   (log/debug (str "generating from spec: " spec))
+                                                                   (try
+                                                                     (process [{:fill-one-language
+                                                                                {:count 1
+                                                                                 :spec spec
+                                                                                 :model small
+                                                                                 }}]
+                                                                              "es")
+                                                                     (catch Exception e
+                                                                       (cond
+                                                                        
+                                                                        ;; TODO: make this conditional on
+                                                                        ;; there being a legitimate reason for the exception -
+                                                                        ;; e.g. the verb is "funzionare" (which takes a non-human
+                                                                        ;; subject), but we're trying to generate with
+                                                                        ;; {:agr {:person :1st or :2nd}}, for which the only lexemes
+                                                                        ;; are human.
+                                                                        true
+                                                                        
+                                                                        (log/warn (str "ignoring exception: " e))
+                                                                        false
+                                                                       (throw e))))
+                                                                   ))
+                                                               [:sing :plur]))))
+                                                     [:1st :2nd :3rd]))))
+                                           (cond (= tense
+                                                    {:synsem {:sem {:aspect :perfect
+                                                                    :tense :past}}})
+                                                 [{:gender :masc}
+                                                  {:gender :fem}]
+                                                 true
+                                                 [:top])))))
+                                 (list {:synsem {:sem {:tense :conditional}}}
+                                       {:synsem {:sem {:tense :future}}}
+                                       {:synsem {:sem {:tense :present}}}
+                                       {:synsem {:sem {:aspect :perfect
+                                                       :tense :past}}}
+                                       )))))
+                 todos)))))
+

2bc8625049b9b5d61c6949b3b9df341615c140f3
Eugene Koontz <ekoontz@hiro-tan.org>
2015-10-10 09:13:56 -0700

    comments about cider-nrepl-handler

--- src/babel/core.clj
+++ src/babel/core.clj
@@ -1,6 +1,12 @@
 (ns babel.core
   (:require
    [babel.workbook :as workbook]
+
+   ;; https://github.com/clojure-emacs/cider#installation
+   ;; commented out because it's not clear how to use below.
+   ;;   [clojure.tools.nrepl.server :as nrepl-server]
+   ;;   [cider.nrepl :refer (cider-nrepl-handler)]
+
    [compojure.core :refer [context defroutes GET PUT POST DELETE ANY]]
    [compojure.handler :as handler]
    [compojure.route :as route]

99aa149d3c4e08b123f26d89805a59daaaec351d
franksfo <franksfo2003@gmail.com>
2015-10-10 15:47:25 -0700

    added cambiarsi

--- src/babel/italiano/lexicon.clj
+++ src/babel/italiano/lexicon.clj
@@ -434,6 +434,21 @@
    "cambiare" {:synsem {:cat :verb
                         :sem {:pred :change}}}
    
+   "cambiarsi" (let [subject-semantics (ref {:animate true})
+                   subject-agr (ref :top)]
+               {:synsem {:cat :verb
+                         :essere true
+                         :sem {:pred :change-clothes
+                               :reflexive true
+                               :subj subject-semantics
+                               :obj subject-semantics}
+                         :subcat {:1 {:agr subject-agr
+                                      :sem subject-semantics}
+                                  :2 {:agr subject-agr
+                                      :pronoun true
+                                      :reflexive true
+                                      :sem subject-semantics}}}})
+   
    "cancellare" {:synsem {:cat :verb
                           :sem {:pred :erase}}}
 

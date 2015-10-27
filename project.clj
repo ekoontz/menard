78b38d235888c85ebfdb9fb88f19055d530d7cb9
Eugene Koontz <ekoontz@hiro-tan.org>
2015-10-27 01:01:33 -0700

    upgrade log4j to 1.2.17

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [hiccup "1.0.5"]
                  [javax.servlet/servlet-api "2.5"]
                  [korma "0.4.1"]
-                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]

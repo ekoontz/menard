363b2ebc4c8e376211812af7b454d95389f8fcc3
Eugene Koontz <ekoontz@hiro-tan.org>
2015-12-24 14:07:49 -0800

    expose 'unify' to workbooks

--- src/babel/workbook.clj
+++ src/babel/workbook.clj
@@ -42,7 +42,7 @@
    [clojure.string :as string]
    [clojure.tools.logging :as log]
    [compojure.core :as compojure :refer [GET PUT POST DELETE ANY]]
-   [dag-unify.core :refer [get-in remove-false strip-refs]]
+   [dag-unify.core :refer [get-in remove-false strip-refs unify]]
    [hiccup.core :refer [html]]
 ))
 

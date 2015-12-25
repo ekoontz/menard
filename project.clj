fcc021c19e04f4f96dea96a1cbb8c2acaa989e1c
Eugene Koontz <ekoontz@hiro-tan.org>
2015-12-24 16:52:12 -0800

    +org/clojure/clojurescript

--- project.clj
+++ project.clj
@@ -16,6 +16,7 @@
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]
                  [org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/data.json "0.2.5"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.clojure/tools.namespace "0.2.11"]

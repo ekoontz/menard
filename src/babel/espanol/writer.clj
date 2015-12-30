0fcc4d6fb807f189b763404574b2404879b4b014
Eugene Koontz <ekoontz@hiro-tan.org>
2015-12-29 16:33:59 -0800

    generate imperfect for spanish

--- src/babel/espanol/writer.clj
+++ src/babel/espanol/writer.clj
@@ -138,6 +138,8 @@
                                  (list {:synsem {:sem {:tense :conditional}}}
                                        {:synsem {:sem {:tense :future}}}
                                        {:synsem {:sem {:tense :present}}}
+                                       {:synsem {:sem {:aspect :progressive
+                                                       :tense :past}}}
                                        {:synsem {:sem {:aspect :perfect
                                                        :tense :past}}}
                                        )))))

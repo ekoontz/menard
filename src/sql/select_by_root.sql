fabaed64b5e2463b82303d76d8431fb387145b3e
Eugene Koontz <ekoontz@hiro-tan.org>
2016-02-24 22:20:16 -0800

    fix cedilla in sql

--- src/sql/select_by_root.sql
+++ src/sql/select_by_root.sql
@@ -5,17 +5,17 @@ CREATE OR REPLACE FUNCTION by_root (in root text)
    	          model,surface,structure,serialized
 FROM expression
 WHERE (expression.language='it' AND expression.structure->'root'->'italiano'->>'italiano' = root)
-   OR (expression.language='fr' AND expression.structure->'root'->'francais'->>'francais' = root)
+   OR (expression.language='fr' AND expression.structure->'root'->'français'->>'français' = root)
    OR (expression.language='es' AND expression.structure->'root'->'espanol'->>'espanol' = root)
    OR (expression.language='en' AND expression.structure->'root'->'english'->>'english' = root)
     $$
     LANGUAGE SQL;
 
-CREATE VIEW exp_with_root
+CREATE OR REPLACE VIEW expression_with_root
     AS SELECT id,created,language,model,surface,structure,
     serialized,COALESCE(
 	structure->'root'->'italiano'->>'italiano',
-        structure->'root'->'francais'->>'francais',
+        structure->'root'->'français'->>'français',
    	structure->'root'->'english'->>'english',
 	structure->'root'->'espanol'->>'espanol') AS root
 FROM expression;

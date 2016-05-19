be26dc246f405db4917df2f59f0ea8e46685348c
Eugene Koontz <ekoontz@hiro-tan.org>
2016-05-19 11:56:07 -0700

    release 1.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject babel "1.3.1-SNAPSHOT"
+(defproject babel "1.4.0"
   :description "A Clojure library for generation of expressions from grammars and lexicons."
   :url "http://github.com/ekoontz/babel"
   :license {:name "Proprietary: all rights reserved. No distribution allowed without consent of owners."}

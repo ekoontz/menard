d53d043a99bb02a2789f5b99edf211dad28f1f87
Eugene Koontz <ekoontz@hiro-tan.org>
2016-05-31 16:30:14 -0700

    whitespace

--- demo.sh
+++ demo.sh
@@ -1,16 +1,21 @@
+
 echo "10 random expressions:"
 lein run -m babel.english/demo 10
 
+echo
 echo "10 random sentences:"
 lein run -m babel.english/demo 10 "{:synsem {:cat :verb}}"
 
+echo
 echo "10 random sentences about eating:"
 lein run -m babel.english/demo 10 "{:synsem {:cat :verb
                                              :sem {:pred :mangiare}}}"
 
+echo
 echo "10 random noun phrases:"
 lein run -m babel.english/demo 10 "{:synsem {:cat :noun}}"
 
+echo
 echo "10 random noun phrases with 'dog':"
 lein run -m babel.english/demo 10 "{:synsem {:cat :noun
                                              :sem {:pred :cane}}}"

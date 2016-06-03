4368d7a716f963dcfe9ed2a473cd2a9ec13c6cef
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-03 10:53:01 -0700

    parameterize number of sentences

--- demo.sh
+++ demo.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-#TODO: run other languages besides english.
-lein run -m babel.english.demo/demo 5
+NUM=$1
 
-lein run -m babel.italiano.demo/demo 5
+if [[ ! $NUM ]]; then
+    NUM=5
+fi
+
+#TODO: run other languages besides english.
+lein run -m babel.english.demo/demo $NUM
+
+lein run -m babel.italiano.demo/demo $NUM
 

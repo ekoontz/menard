0298fc02d029c2ec744a9f7a0e1560c4cefb75ed
Eugene Koontz <ekoontz@hiro-tan.org>
2016-08-18 16:14:39 -0700

    add stuff to lab

--- src/babel/italiano/lab.clj
+++ src/babel/italiano/lab.clj
@@ -9,3 +9,16 @@
                                                (catch Exception e (log/error (str "failed to generate."))))]
                  (if result
                    (println (fo result))))))
+
+(defn sleep []
+  (repeatedly #(println (fo (generate {:synsem {:cat :verb
+                                                             :essere false
+                                                :sem {:pred :sleep
+                                                      :tense :conditional}}}
+                                      :truncate false
+                                      :model medium)))))
+
+(defn nps []
+  (repeatedly #(println (fo (generate {:synsem {:cat :noun}}
+                                      :truncate false
+                                      :model medium)))))

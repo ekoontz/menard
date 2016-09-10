f516b1dab2a31160c76ef2b29be5bcde07f7b663
Eugene Koontz <ekoontz@hiro-tan.org>
2016-09-10 12:54:03 -0700

    upgrade dag_unify

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
   :dependencies [[clj-time "0.7.0"]
                  [clojail "1.0.6"]
                  [compojure "1.1.6"]
-                 [dag_unify "1.3.6-SNAPSHOT"]
+                 [dag_unify "1.3.6"]
                  [environ "1.0.0"]
                  [hiccup "1.0.5"]
                  [javax.servlet/servlet-api "2.5"]

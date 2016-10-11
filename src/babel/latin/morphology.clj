40982c33f5460607bd4f2f694f00f723109898ac
Eugene Koontz <ekoontz@hiro-tan.org>
2016-10-11 14:09:31 -0700

    +starting latin morphology: present-tense only

---
+++ src/babel/latin/morphology.clj
@@ -0,0 +1,63 @@
+(ns babel.latin.morphology
+  )
+
+(def replace-patterns
+  {:p [#"^(.)eo$" "$1eo"]
+   :g [#"^(.)eo$" "$1eo"]
+   :u {:synsem {:infl :present
+                :sem {:tense :present}
+                :subcat {:1 {:agr {:number :sing
+                                   :person :1st}}}}}}
+  {:p [#"^(.)as$" "$1eo"]
+   :g [#"^(.)eo$" "$1as"]
+   :u {:synsem {:infl :present
+                :sem {:tense :present}
+                :subcat {:1 {:agr {:number :sing
+                                   :person :2nd}}}}}}
+  {:p [#"^(.)at$" "$1eo"]
+   :g [#"^(.)eo$" "$1at"]
+   :u {:synsem {:infl :present
+                :sem {:tense :present}
+                :subcat {:1 {:agr {:number :sing
+                                   :person :3rd}}}}}}
+  {:p [#"^(.)amus$" "$1eo"]
+   :g [#"^(.)eo$" "$1amus"]
+   :u {:synsem {:infl :present
+                :sem {:tense :present}
+                :subcat {:1 {:agr {:number :plur
+                                   :person :1st}}}}}}
+  {:p [#"^(.)atis$" "$1eo"]
+   :g [#"^(.)eo$" "$1atis"]
+   :u {:synsem {:infl :present
+                :sem {:tense :present}
+                :subcat {:1 {:agr {:number :plur
+                                   :person :2nd}}}}}}
+  {:p [#"^(.)ant$" "$1eo"]
+   :g [#"^(.)eo$" "$1ant"]
+   :u {:synsem {:infl :present
+                :sem {:tense :present}
+                :subcat {:1 {:agr {:number :plur
+                                   :person :3rd}}}}}})
+   
+(defn analyze [surface-form lexicon]
+  "Analyze a single surface form into a set of lexical forms."
+  )
+
+(defn conjugate [infinitive unify-with]
+  "Conjugate an infinitive into a surface form by taking the first 
+   element of replace-patterns where the element's :u unifies successfully with
+   unify-with."
+  (first
+   (take 1
+         (remove #(nil? %)
+                 (map
+                  (fn [replace-pattern]
+                    (let [from (nth (:g replace-pattern) 0)
+                          to (nth (:g replace-pattern) 1)
+                          unify-against (or (:u replace-pattern) :top)]
+                      (if (and from to
+                               (re-matches from infinitive)
+                               (not (fail? (unifyc unify-against
+                                                   unify-with))))
+                        (string/replace infinitive from to))))
+                  replace-patterns)))))

b3c77d40658207e97f9d7a0fcf78aa6430329464
Eugene Koontz <ekoontz@hiro-tan.org>
2016-10-23 09:18:19 -0700

    release 2.2.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject babel "2.2.7-SNAPSHOT"
+(defproject babel "2.2.7"
   :description "A library for natural language generation and parsing"
   :url "http://github.com/ekoontz/babel"
   :license {:name "Eclipse Public License"

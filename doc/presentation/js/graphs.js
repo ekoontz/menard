69748f502283054476f4156e18d0660612aed8b7
Eugene Koontz <ekoontz@hiro-tan.org>
2016-10-27 21:21:31 -0700

    working on incremental-generation slides

--- doc/presentation/js/graphs.js
+++ doc/presentation/js/graphs.js
@@ -1,109 +1,3 @@
-var depth_first_0 = `
-
-digraph {
-    
-    node [rx=5 ry=5 labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
-
-    edge [labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
-
-    she_sees_the_cat [labelType="html"
-       label="<div class='avm hide'>
-                 <table>
-                    <tr>
-                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
-                    </tr>
-                  </table></div>"]; 
-
-    she [labelType="html"
-       label="<div class='avm hide'>
-                 <table>
-                    <tr>
-                      <th>sem</th><td>‍👱‍♀️</td>
-                    </tr>
-                    <tr>
-                      <th>str</th><td><i>she</i></td>
-                    </tr>
-                  </table></div>"]; 
-
-    sees_the_cat [labelType="html"
-       label="<div class='avm hide'>
-                 <table>
-                    <tr>
-                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
-                    </tr>
-                  </table></div>"]; 
-
-    she_sees_the_cat -> she          [label="arg" labelStyle="fill: #55f; font-weight: bold;"];
-    she_sees_the_cat -> sees_the_cat [label="f()" labelStyle="fill: #55f; font-weight: bold;"];
-}
-
-`;
-
-var depth_first_05 = `
-
-digraph {
-    
-    node [rx=5 ry=5 labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
-
-    edge [labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
-
-    she_sees_the_cat [labelType="html"
-       label="<div class='avm hide'>
-                 <table>
-                    <tr>
-                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
-                    </tr>
-                  </table></div>"]; 
-
-    she [labelType="html"
-       label="<div class='avm hide'>
-                 <table>
-                    <tr>
-                      <th>sem</th><td>‍👱‍♀️</td>
-                    </tr>
-                    <tr>
-                      <th>str</th><td><i>she</i></td>
-                    </tr>
-                  </table></div>"]; 
-
-    sees_the_cat [labelType="html"
-       label="<div class='avm hide'>
-                 <table>
-                    <tr>
-                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
-                    </tr>
-                  </table></div>"]; 
-
-    sees [labelType="html"
-       label="<div class='avm hide'>
-                 <table>
-                    <tr>
-                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
-                    </tr>
-                    <tr>
-                      <th>str</th><td><i>sees</i></td>
-                    </tr>
-                  </table></div>"]; 
-
-    the_cat [labelType="html"
-       label="<div class='avm hide'>
-                 <table>
-                    <tr>
-                      <th>sem</th><td>‍🐱</td>
-                    </tr>
-                    <tr>
-                      <th>str</th><td><i>the cat</i></td>
-                    </tr>
-                  </table></div>"]; 
-
-    she_sees_the_cat -> she          [label="arg" labelStyle="fill: #55f; font-weight: bold;"];
-    she_sees_the_cat -> sees_the_cat [label="f()" labelStyle="fill: #55f; font-weight: bold;"];
-    sees_the_cat     -> sees         [label="f()" labelStyle="fill: #55f; font-weight: bold;"];
-    sees_the_cat     -> the_cat      [label="arg" labelStyle="fill: #55f; font-weight: bold;"];
-}
-
-`;
-
 var depth_first_1 = `
 
 digraph {
@@ -139,6 +33,112 @@ digraph {
                     </tr>
                   </table></div>"]; 
 
+    she_sees_the_cat -> she          [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    she_sees_the_cat -> sees_the_cat [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+}
+
+`;
+
+var depth_first_2 = `
+
+digraph {
+    
+    node [rx=5 ry=5 labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
+
+    edge [labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
+
+    she_sees_the_cat [labelType="html"
+       label="<div class='avm hide'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
+                    </tr>
+                  </table></div>"]; 
+
+    she [labelType="html"
+       label="<div class='avm hide'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td>‍👱‍♀️</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>she</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    sees_the_cat [labelType="html"
+       label="<div class='avm hide'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
+                    </tr>
+                  </table></div>"]; 
+
+    sees [labelType="html"
+       label="<div class='avm hide'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>sees</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    the_cat [labelType="html"
+       label="<div class='avm hide'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td>‍🐱</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>the cat</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    she_sees_the_cat -> she          [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    she_sees_the_cat -> sees_the_cat [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    sees_the_cat     -> sees         [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    sees_the_cat     -> the_cat      [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+}
+
+`;
+
+var depth_first_3 = `
+
+digraph {
+    
+    node [rx=5 ry=5 labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
+
+    edge [labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
+
+    she_sees_the_cat [labelType="html"
+       label="<div class='avm hide'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
+                    </tr>
+                  </table></div>"]; 
+
+    she [labelType="html"
+       label="<div class='avm hide'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td>‍👱‍♀️</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>she</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    sees_the_cat [labelType="html"
+       label="<div class='avm hide'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
+                    </tr>
+                  </table></div>"]; 
+
     sees [labelType="html"
        label="<div class='avm'>
                  <table>
@@ -161,15 +161,15 @@ digraph {
                     </tr>
                   </table></div>"]; 
 
-    she_sees_the_cat -> she          [label="arg" labelStyle="fill: #55f; font-weight: bold;"];
-    she_sees_the_cat -> sees_the_cat [label="f()" labelStyle="fill: #55f; font-weight: bold;"];
-    sees_the_cat     -> sees         [label="f()" labelStyle="fill: #55f; font-weight: bold;"];
-    sees_the_cat     -> the_cat      [label="arg" labelStyle="fill: #55f; font-weight: bold;"];
+    she_sees_the_cat -> she          [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    she_sees_the_cat -> sees_the_cat [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    sees_the_cat     -> sees         [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    sees_the_cat     -> the_cat      [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
 }
 
 `;
 
-var she_sees_the_cat = `
+var depth_first_4 = `
 
 digraph {
     
@@ -178,18 +178,15 @@ digraph {
     edge [labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
 
     she_sees_the_cat [labelType="html"
-       label="<div class='avm'>
+       label="<div class='avm hide'>
                  <table>
                     <tr>
-                      <th>sem</th><td> 👁 (👱‍♀️,🐱)</td>
-                    </tr>
-                    <tr>
-                      <th>str</th><td><i>she sees the cat</i></td>
+                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
                     </tr>
                   </table></div>"]; 
 
     she [labelType="html"
-       label="<div class='avm'>
+       label="<div class='avm hide'>
                  <table>
                     <tr>
                       <th>sem</th><td>‍👱‍♀️</td>
@@ -200,13 +197,10 @@ digraph {
                   </table></div>"]; 
 
     sees_the_cat [labelType="html"
-       label="<div class='avm'>
+       label="<div class='avm hide'>
                  <table>
                     <tr>
-                      <th>sem</th><td> ‍👁 ( _ ,🐱)</td>
-                    </tr>
-                    <tr>
-                      <th>str</th><td><i>sees the cat</i></td>
+                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
                     </tr>
                   </table></div>"]; 
 
@@ -222,7 +216,7 @@ digraph {
                   </table></div>"]; 
 
     the_cat [labelType="html"
-       label="<div class='avm'>
+       label="<div class='avm hide'>
                  <table>
                     <tr>
                       <th>sem</th><td>‍🐱</td>
@@ -254,17 +248,43 @@ digraph {
                     </tr>
                   </table></div>"]; 
     
-    she_sees_the_cat  -> she;
-    she_sees_the_cat  -> sees_the_cat;
-    sees_the_cat      -> sees;
-    sees_the_cat      -> the_cat;
-    the_cat           -> the;
-    the_cat           -> cat;
+    she_sees_the_cat -> she          [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    she_sees_the_cat -> sees_the_cat [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    sees_the_cat     -> sees         [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    sees_the_cat     -> the_cat      [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+
+    the [labelType="html"
+       label="<div class='avm hide'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td></td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>the</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    cat [labelType="html"
+       label="<div class='avm hide'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td>🐱</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>cat</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    the_cat           -> the       [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+    the_cat           -> cat       [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
+
+    
 }
 
 `;
 
-var depth_first_2 = `
+
+var depth_first_5 = `
 
 digraph {
     
@@ -374,6 +394,101 @@ digraph {
     the_cat           -> cat       [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:300%"];
 
     
+}
+
+`;
+
+var she_sees_the_cat = `
+
+digraph {
+    
+    node [rx=5 ry=5 labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
+
+    edge [labelStyle="font: 300 14px 'Helvetica Neue', Helvetica"];
+
+    she_sees_the_cat [labelType="html"
+       label="<div class='avm'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td> 👁 (👱‍♀️,🐱)</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>she sees the cat</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    she [labelType="html"
+       label="<div class='avm'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td>‍👱‍♀️</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>she</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    sees_the_cat [labelType="html"
+       label="<div class='avm'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td> ‍👁 ( _ ,🐱)</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>sees the cat</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    sees [labelType="html"
+       label="<div class='avm'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td> ‍👁 ( _ , _ )</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>sees</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    the_cat [labelType="html"
+       label="<div class='avm'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td>‍🐱</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>the cat</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    the [labelType="html"
+       label="<div class='avm'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td></td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>the</i></td>
+                    </tr>
+                  </table></div>"]; 
+
+    cat [labelType="html"
+       label="<div class='avm'>
+                 <table>
+                    <tr>
+                      <th>sem</th><td>🐱</td>
+                    </tr>
+                    <tr>
+                      <th>str</th><td><i>cat</i></td>
+                    </tr>
+                  </table></div>"]; 
+    
+    she_sees_the_cat  -> she          [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:100%"];
+    she_sees_the_cat  -> sees_the_cat [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:100%"];
+    sees_the_cat      -> sees [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:100%"];
+    sees_the_cat      -> the_cat [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:100%"];
+    the_cat           -> the [label="arg" labelStyle="fill: #55f; font-weight: bold; font-size:100%"];
+    the_cat           -> cat [label="f()" labelStyle="fill: #55f; font-weight: bold; font-size:100%"];
 }
 
 `;

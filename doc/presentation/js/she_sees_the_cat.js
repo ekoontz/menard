6e3b2cc2c7fb0a14d7beb2681ce869bf73b9082e
Eugene Koontz <ekoontz@hiro-tan.org>
2016-10-27 20:19:36 -0700

    refactor svg rendering

--- doc/presentation/js/she_sees_the_cat.js
+++ doc/presentation/js/she_sees_the_cat.js
@@ -1,17 +1,2 @@
-var syntax;
-try {
-    syntax = graphlibDot.read(she_sees_the_cat);
-} catch (e) {
-    console.error("could not read input graph:" + e);
-}
-
-syntax.graph().transition = function(selection) {
-    return selection.transition().duration(500);
-};
-
-// Create and configure the renderer
-var render = dagreD3.render();
-
-// Render the graph into svg g
-var svg = d3.select("#she_sees_the_cat").call(render, syntax);
+var svg = d3.select("#she_sees_the_cat").call(dagreD3.render(), graphlibDot.read(she_sees_the_cat));
 

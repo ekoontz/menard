656831e1e6b501924d352156d65824e97f7aaa35
Eugene Koontz <ekoontz@hiro-tan.org>
2017-02-25 23:23:47 -0800

    start working on 2.7.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject babel "2.7.0"
+(defproject babel "2.7.1-SNAPSHOT"
   :description "A library for natural language generation and parsing"
   :url "http://github.com/ekoontz/babel"
   :license {:name "Eclipse Public License"

0c11160697534f1d1711627a2e31629aec0b4ba4
Eugene Koontz <ekoontz@hiro-tan.org>
2017-03-09 01:40:48 -0800

    improve documentation in shell script that generates sentences

--- all.sh
+++ all.sh
@@ -6,10 +6,18 @@ lein run -m babel.english.writer/translate "es"
 lein run -m babel.francais.writer/tout
 lein run -m babel.english.writer/translate "fr"
 
-# 1. only generate if needed (if no existing expressions are in the database)
-lein run -m babel.italiano.writer/tutti
-lein run -m babel.english.writer/translate "it"
+# If you set CHECK_EXISTING="false" then generate no matter what
+# (ignore existing expressions, if any.
+# Otherwise, only generate if needed (if no existing expressions
+# are in the database)
 
-# 2. generate no matter what (ignore existing expressions, if any)
-#lein run -m babel.italiano.writer/tutti "all" "all" "$(date)"
-#lein run -m babel.english.writer/translate "it" "all" "$(date)"
+if [ "${CHECK_EXISTING}" != "false" ]; then
+    lein run -m babel.italiano.writer/tutti
+    lein run -m babel.english.writer/translate "it"
+else
+    lein run -m babel.italiano.writer/tutti "all" "all" "$(date)"
+    lein run -m babel.english.writer/translate "it" "all" "$(date)"
+fi
+
+
+    

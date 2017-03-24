267a231a17d2ed243fbabe012e042252c7d38926
Eugene Koontz <ekoontz@hiro-tan.org>
2017-03-23 23:02:44 -0700

    remove files that should not have been committed

--- src/scripts/expressions_dev_dump.sh
+++
@@ -1,1 +0,0 @@
-pg_dump --no-acl --no-owner -h ec2-54-204-15-41.compute-1.amazonaws.com -U lfytngdoqpbjna -t expression --data-only deh8rf6ih1is59 | gzip > expressions.db.gz
--- src/scripts/expressions_prod_dump.sh
+++
@@ -1,1 +0,0 @@
-pg_dump --no-acl --no-owner -h ec2-50-16-187-40.compute-1.amazonaws.com -U u5mihksetbhvef -t expression --data-only d6vssddqsl11up | gzip > expressions.db.gz

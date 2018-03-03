4fb843d7171d0d306ec858b3e1bda8963d50d701
Eugene Koontz <ekoontz@hiro-tan.org>
2018-03-02 21:25:20 -0800

    move (init-db) out of transaction to prevent sequence-already-existing-exceptions causing transaction to fail

--- src/scripts/update-lexemes.sh
+++ src/scripts/update-lexemes.sh
@@ -1,20 +1,20 @@
 echo "(do (use 'babel.directory)
           (use 'babel.korma)
           (use 'korma.db)
-        (init-db)
-        (transaction
-          (print (str \"it..\"))
-          (println
-             (babel.italiano.grammar/write-lexicon))
+          (init-db)
+          (transaction
+            (print (str \"it..\"))
+            (println
+              (babel.italiano.grammar/write-lexicon))
 
-          (print (str \"en..\"))
-          (println 
-             (babel.english.grammar/write-lexicon))
+            (print (str \"en..\"))
+            (println 
+              (babel.english.grammar/write-lexicon))
 
-          (print (str \"es..\"))
-          (println
-	     (babel.espanol.grammar/write-lexicon))
+            (print (str \"es..\"))
+            (println
+  	      (babel.espanol.grammar/write-lexicon))
 
-          (print (str \"fr..\"))
-	  (println (babel.francais.grammar/write-lexicon))))" | lein repl
+            (print (str \"fr..\"))
+	    (println (babel.francais.grammar/write-lexicon))))" | lein repl
 

b954f23f35fdbcf1950e6a0c6112b00e9ee3ae30
Eugene Koontz <ekoontz@hiro-tan.org>
2018-08-30 09:10:32 -0700

    remove debugging println

--- src/babel/reader.clj
+++ src/babel/reader.clj
@@ -145,8 +145,7 @@
                    (filter lexical-filter-fn)))})
         target-expression
         (target-timing-fn
-         (binding [babel.generate/truncate? true
-                   babel.generate/println? true]
+         (binding [babel.generate/truncate? true]
            (babel.generate/generate target-spec target-model)))
         source-spec
         (u/strip-refs ;; TODO: consider removing strip-refs; not clear if there is any reason why we need to do it.

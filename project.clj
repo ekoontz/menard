8ced83adc82e1954f3791973237f36b6307b578e
Eugene Koontz <ekoontz@hiro-tan.org>
2019-01-20 10:36:46 -0800

    cleanup dependencies

--- project.clj
+++ project.clj
@@ -1,53 +1,17 @@
-(defproject babel "0.0.1-SNAPSHOT"
+(defproject babylon "0.0.1-SNAPSHOT"
   :description "A library for natural language generation and parsing"
   :url "http://github.com/ekoontz/babylon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cljstache "2.0.0"]
-                 [clj-time "0.11.0"]
-                 [clojail "1.0.6"]
-                 [compojure "1.1.6"]
-                 [dag_unify "1.6.5"]
-                 [environ "1.0.0"]
-                 [korma "0.4.3"]
+  :dependencies [[dag_unify "1.6.5"]
                  [log4j/log4j "1.2.17"]
-                 [org.clojure/core.cache "0.7.1"]
                  [org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [org.clojure/core.async "0.4.474"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/math.combinatorics "0.1.3"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [org.clojure/tools.namespace "0.2.11"]
-                 [org.postgresql/postgresql "42.1.4"]]
-
-  :filespecs [{:type :path :path "compiled"}]
+                 [org.clojure/tools.logging "0.4.0"]]
   :resource-paths ["resources"]
   :plugins [[cider/cider-nrepl "0.18.0"]
-            [lein-cljsbuild "1.1.2"]
-            [lein-doo "0.1.6"]
             [lein-environ "1.0.0"]
             [lein-localrepo "0.4.0"]
-            [lein-pprint "1.1.1"]
-            [lein-ring "0.9.7"]]
-
-  ;; run clojure tests with "lein test"
-  ;; run clojurescript tests with "lein doo slimer test"
-  :cljsbuild {:builds [{:id "test"
-                        :source-paths ["src" "test"]
-                        :compiler {:output-to "out/testable.js"
-                                   ;; you must have {:optimizations :whitespace}
-                                   ;; to avoid "ReferenceError: Can't find variable: goog"
-                                   :optimizations :whitespace}}]}
-  :doo {:paths {:phantom "phantomjs --debug=true --web-security=false  --disk-cache=true --webdriver=127.0.0.1:8910"
-                :slimer "slimerjs --ignore-ssl-errors=true"
-                :karma "karma --port=9881 --no-colors"
-                :rhino "java -jar /Users/ekoontz/Downloads/rhino1_7R4/js-14.jar -strict"
-                :node "node --trace-gc --trace-gc-verbose"}}
-  
-  :ring {:handler babylon.core/app})
-;; this hook doesn't work yet.
-;;  :hooks [leiningen.cljsbuild])
+            [lein-pprint "1.1.1"]])
 
 
 

142feff8df570405e7e0f0acf24fbbc3b9308eae
Eugene Koontz <ekoontz@hiro-tan.org>
2019-01-20 11:05:17 -0800

    upgrade clojure to latest release

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[dag_unify "1.6.5"]
                  [log4j/log4j "1.2.17"]
-                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.logging "0.4.0"]]
   :resource-paths ["resources"]
   :plugins [[cider/cider-nrepl "0.18.0"]

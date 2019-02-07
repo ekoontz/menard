6e18e1f3c5c99bb5ff4dd855a1d8e7463022221b
Eugene Koontz <ekoontz@hiro-tan.org>
2019-02-07 09:10:20 -0800

    +defn a-specific-noun-phrase-2

--- src/babylon/english/lab.clj
+++ src/babylon/english/lab.clj
@@ -15,4 +15,21 @@
             {:st (syntax-tree my-bespoke-noun-phrase)
              :morph (morph my-bespoke-noun-phrase)
              :phrase? (u/get-in my-bespoke-noun-phrase [:head :comp :phrasal])}]
+      (:st some-structural-info))))
+
+(defn a-specific-noun-phrase-2
+  "how to generate a noun phrase with particular constraints."
+  []
+  (let [my-bespoke-noun-phrase
+        (generate {:phrasal true
+                   :rule "np"
+                   :head {:rule "nbar3" :phrasal true
+                          :comp {:phrasal true
+                                 :rule "comp1"
+                                 :comp {:phrasal true
+                                        :rule "s-slash"}}}})]
+    (let [some-structural-info
+            {:st (syntax-tree my-bespoke-noun-phrase)
+             :morph (morph my-bespoke-noun-phrase)
+             :phrase? (u/get-in my-bespoke-noun-phrase [:head :comp :phrasal])}]
       (:morph some-structural-info))))

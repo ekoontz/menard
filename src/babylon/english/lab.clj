afe3b006660020af7c9c3464b397d17a86ba6273
Eugene Koontz <ekoontz@hiro-tan.org>
2019-05-06 12:21:17 -0700

    ~generate,~demo: parameterize input spec for generation

--- src/babylon/english/lab.clj
+++ src/babylon/english/lab.clj
@@ -357,36 +357,32 @@
     true
     (en/morph syntax-tree)))
 
-(defn generate []
+(defn generate [spec]
   (->>
-   ;; 1. start with a list containing a single empty tree:
-   [{}]
-
-   ;; 2. start with s at the root level:
-   (g/lazy-mapcat #(add-rule % "s"))
-
-   ;; 3. add s -> .comp *_vp-aux_
-   (g/lazy-mapcat #(add-rule % "vp-aux"))
-
-   ;; 4. add auxiliary verb: vp-aux -> *v .comp
+   [spec]
+   (g/lazy-mapcat add-rule)
+   (g/lazy-mapcat add-rule)
    (g/lazy-mapcat add-lexeme)
-
-   ;; 5. add vp: *vp-aux -> *v _.vp_
-   (g/lazy-mapcat #(add-rule %))
-
-   ;; add .vp -> *_verb_ .comp:
+   (g/lazy-mapcat add-rule)
    (g/lazy-mapcat add-lexeme)
-   
-   ;; 6. add lower complement np: .vp -> *v _.np_:
-   (g/lazy-mapcat #(add-rule %))
+   (g/lazy-mapcat add-rule)
    (g/lazy-mapcat add-lexeme)
    (g/lazy-mapcat add-lexeme)
-
-   ;; 7. add upper complement:np: s -> vp _np_.
-   (g/lazy-mapcat #(add-rule %))
+   (g/lazy-mapcat add-rule)
    (g/lazy-mapcat add-lexeme)
    (g/lazy-mapcat add-lexeme)))
 
+(def demo-spec
+  (-> {:rule "s"
+       :comp {:rule "np"}
+       :head {:rule "vp-aux"
+              :comp {:rule "vp"
+                     :comp {:rule "np"}}}}))
 (defn demo []
-  (repeatedly #(println (morph (time (-> (generate) first))))))
-
+  (repeatedly
+   #(println
+     (-> demo-spec
+         generate
+         first
+         time
+         syntax-tree))))

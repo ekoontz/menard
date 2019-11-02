2a72cc631ff7e3a4e08d9f54c6cdee0753578963
Eugene Koontz <ekoontz@hiro-tan.org>
2019-11-02 21:50:58 +0100

    upgrade libraries to latest releases

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/ekoontz/babylon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[dag_unify "1.7.0-SNAPSHOT"]
+  :dependencies [[dag_unify "1.7.1"]
                  [log4j/log4j "1.2.17"]
                  [org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.4.500"]
-                 [org.clojure/tools.logging "0.4.0"]]
+                 [org.clojure/tools.logging "0.5.0"]]
   :resource-paths ["resources"])
 
 

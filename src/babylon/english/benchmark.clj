4757550cf918990f8454411d9e743073b06a1030
Eugene Koontz <ekoontz@hiro-tan.org>
2019-11-02 21:52:46 +0100

    whitespace

--- src/babylon/english/benchmark.clj
+++ src/babylon/english/benchmark.clj
@@ -10,28 +10,37 @@
    :comp {:rule "np"
           :head {:phrasal true}}
    :head {:rule "vp"
-          :head {:subcat {:1 {:cat :noun}
-                          :2 {:cat :noun}
-                          :3 []}}
-          :comp {:rule "nbar4"
-                 :phrasal true
-                 :head {:phrasal false
-                        :canonical :top}
-                 :comp {:phrasal true
-                        :rule "comp1"
-                        :comp {:phrasal true
-                               :rule "s-slash"
-                               :comp {:phrasal true
-                                      :rule "np"
-                                      :head {:phrasal true}
-                                      :comp {:phrasal false}}
-                               :head {:phrasal true
-                                      :rule "vp-aux-slash"
-                                      :head {:phrasal false
-                                             :aux true
-                                             :subcat {:2 {:modal false}}}
-                                      :comp {:phrasal (or false false) ;; changing this to true will take a long time and finally you'll get a 'dead end' error.
-                                             :subcat {:1 {:cat :noun}}}}}}}}})
+          :head {:phrasal false
+                 :subcat {:2 {:cat :comp
+                              :rule "comp2"
+                              :phrasal true
+                              :comp
+                              {:rule "s"
+                               :comp {:rule "np"
+                                      :head {:phrasal true}}
+                               :head {:rule "vp"
+                                      :head {:subcat {:1 {:cat :noun}
+                                                      :2 {:cat :noun}
+                                                      :3 []}}
+                                      :comp {:rule "nbar4"
+                                             :phrasal true
+                                             :head {:phrasal false
+                                                    :canonical :top}
+                                             :comp {:phrasal true
+                                                    :rule "comp1"
+                                                    :comp {:phrasal true
+                                                           :rule "s-slash"
+                                                           :comp {:phrasal true
+                                                                  :rule "np"
+                                                                  :head {:phrasal true}
+                                                                  :comp {:phrasal false}}
+                                                           :head {:phrasal true
+                                                                  :rule "vp-aux-slash"
+                                                                  :head {:phrasal false
+                                                                         :aux true
+                                                                         :subcat {:2 {:modal false}}}
+                                                                  :comp {:phrasal (or false false) ;; changing this to true will take a long time and finally you'll get a 'dead end' error.
+                                                                         :subcat {:1 {:cat :noun}}}}}}}}}}}}}})
 
 (defn benchmark-line []
   (try

391f34ff5a8361fb18e95764f1954ab05a154c83
Eugene Koontz <ekoontz@hiro-tan.org>
2020-04-11 00:12:20 +0200

    use latest dag_unify release

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/ekoontz/babylon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[dag_unify "1.9.1-SNAPSHOT"]
+  :dependencies [[dag_unify "1.9.1"]
                  [log4j/log4j "1.2.17"]
                  [org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "0.5.0"]

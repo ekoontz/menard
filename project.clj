c146b7d2eb1900e74683306cd7d947b550403350
Eugene Koontz <ekoontz@hiro-tan.org>
2020-07-07 09:04:02 +0200

    prevent parsing of e.g. "jasen" (should be "jassen")

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject menard "1.0.1"
+(defproject menard "1.0.2-SNAPSHOT"
   :description "A library for natural language generation and parsing"
   :url "http://github.com/ekoontz/menard"
   :license {:name "Eclipse Public License"

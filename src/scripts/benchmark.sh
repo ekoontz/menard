0b5ce82ea8cc85d778b674886c7ef9e294c4fc0c
Eugene Koontz <ekoontz@hiro-tan.org>
2020-11-15 09:49:04 +0100

    benchmakring script

---
+++ src/scripts/benchmark.sh
@@ -0,0 +1,1 @@
+echo "(load \"menard/nederlands\")(take 10 (repeatedly #(->> (-> \"een aardig bedroefd buitengewoon afzonderlijk dik nieuwswaardig ongerust teleurgesteld eigenwijs geheim verrast prachtig slim vrouw probeert mannen te zien\" menard.nederlands/parse time) (map menard.nederlands/syntax-tree) (map println))))" | lein repl

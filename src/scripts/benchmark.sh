9813d991135eebad1710b1c10a3d25dc4f058161
Eugene Koontz <ekoontz@hiro-tan.org>
2020-11-24 23:17:13 +0100

    latest stats

--- src/scripts/benchmark.sh
+++ src/scripts/benchmark.sh
@@ -1,20 +1,22 @@
 # Parsing
 
 #               bogomips | price      | ms/parse
-# Mac                                 | 2700
-# t2.micro                            | 9000
+# Mac                                 | 1700
+# t2.micro                            | 6600
 # t3a.xlarge    | 4399.54  | $0.15/hour | 4200
 # c4.xlarge     | 5800     | $0.10/hour | 3800
 # c5n.xlarge    |          | $0.21/hour | 3500
+# c6g.2xlarge   |                         1700
+# c6g.12xlarge  |                          726
 # c6gd.medium   |          | $0.04/hour | 8300
 # c6gd.large    |          | $0.08/hour | 4600
-# c6gd.xlarge   |            $0.15/hour | 2900
+# c6gd.xlarge   |            $0.15/hour | 2765
 # c6gd.2xlarge  |            $0.30/hour | 2166
 # c6gd.4xlarge  |            $0.60/hour | 1530
 # c6gd.8xlarge  |            $1.20/hour | 1250 
-# c6gd.12xlarge |                       | 1070
+# c6gd.12xlarge |                       |  710
 # c6gd.16xlarge |                       | 1070
-# t4g.2xlarge   |            $0.26/hour | 2100
+# t4g.2xlarge   |            $0.26/hour | 1152
 # a1.4xlarge    |                       | 3300
 
 sudo yum -y install java-1.8.0-openjdk git

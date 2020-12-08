11291af36a253c6329197250c40ba933c8975225
Eugene Koontz <ekoontz@hiro-tan.org>
2020-12-08 09:14:33 +0100

    fix path to '(load)'

--- src/scripts/benchmark.sh
+++ src/scripts/benchmark.sh
@@ -19,4 +19,4 @@ mkdir ~/bin
 curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > ~/bin/lein
 chmod 755 ~/bin/lein
 export PATH=$PATH:~/bin
-echo "(load \"menard/nederlands\")(take 10 (repeatedly #(->> (-> \"een aardig bedroefd buitengewoon afzonderlijk dik nieuwswaardig ongerust teleurgesteld eigenwijs geheim verrast prachtig slim vrouw probeert mannen te zien\" menard.nederlands/parse time) (map menard.nederlands/syntax-tree) (map println))))" | lein repl
+echo "(load \"nederlands\")(take 10 (repeatedly #(->> (-> \"een aardig bedroefd buitengewoon afzonderlijk dik nieuwswaardig ongerust teleurgesteld eigenwijs geheim verrast prachtig slim vrouw probeert mannen te zien\" menard.nederlands/parse time) (map menard.nederlands/syntax-tree) (map println))))" | lein repl

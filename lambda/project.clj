df2b7b2eb5bb5eef1fe7613504bb595a5eb7179c
Eugene Koontz <ekoontz@hiro-tan.org>
2021-04-04 13:16:04 +0200

    use latest release of holy-lambda instead of a snapshot

--- lambda/project.clj
+++ lambda/project.clj
@@ -1,6 +1,6 @@
 (defproject nlquiz-lambda "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.2-rc1"]
-                 [fierycod/holy-lambda "0.0.7-SNAPSHOT"]
+                 [fierycod/holy-lambda "0.0.7"]
                  [menard "1.4.1-SNAPSHOT"]]
   :global-vars {*warn-on-reflection* true}
   :main ^:skip-aot nlquiz-lambda.core

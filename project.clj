bebd4fc9731f14e22d28b59ab2e64207feba12cf
Eugene Koontz <ekoontz@hiro-tan.org>
2021-11-28 10:56:57 +0100

    update clojure to latest release

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[dag_unify "1.10.0"]
                  [log4j/log4j "1.2.17"]
-                 [org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojure "1.10.3"]
                  [org.clojure/data.json "1.0.0"]
                  [org.clojure/tools.logging "1.1.0"]
                  [cljslog "0.1.0"]

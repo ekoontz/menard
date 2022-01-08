e32dc34ffd5f571b6b29abe7ba8e4bc71d49c6f0
Eugene Koontz <ekoontz@hiro-tan.org>
2022-01-09 00:54:07 +0100

    upgrade to snapshot version of dag_unify and latest tools.logging

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/ekoontz/menard"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[dag_unify "1.10.0"]
+  :dependencies [[dag_unify "1.10.1-SNAPSHOT"]
                  [log4j/log4j "1.2.17"]
                  [org.clojure/clojure "1.10.3"]
                  [org.clojure/data.json "1.0.0"]
-                 [org.clojure/tools.logging "1.2.2"]
+                 [org.clojure/tools.logging "1.2.4"]
                  [cljslog "0.1.0"]
                  [clojure.java-time "0.3.3"]]
   :resource-paths ["resources"])

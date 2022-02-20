3d225ac330956975c9a2ffaa9edf08fb21ddf533
Eugene Koontz <ekoontz@hiro-tan.org>
2022-02-20 22:21:06 +0100

    add script to start the server

---
+++ server/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Start the language endpoint server in the background. This will generate
+# expressions for the user and parse their responses. It listens on port
+# 3000, where it handles requests to:
+# - generate expressions for the user
+# - parse the user's guesses of answers to those expressions
+# - provides various linguistic resources (lexicons,
+#   grammars and morphological rules).
+#
+# The LANGUAGE_ENDPOINT_URL is the URL for the UI server
+# It must be provided as an environmental variable to this
+# script. The language endpoint needs that URL so it can set the
+# header: 'Access-Control-Allow-Origin' to that.
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd ${SCRIPT_DIR}
+
+echo "** start.sh ** starting the server from directory: ${SCRIPT_DIR} and with hostname: ${HOSTNAME}"
+
+ORIGIN=http://${HOSTNAME}:3449 lein ring server-headless

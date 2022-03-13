5b0b6c762134d70fc53104b0d96d8a69c3070398
Eugene Koontz <ekoontz@hiro-tan.org>
2022-03-13 21:39:05 +0100

    update menard to latest available release

--- lambda/project.clj
+++ lambda/project.clj
@@ -1,7 +1,7 @@
 (defproject menard-lambda "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [fierycod/holy-lambda "0.0.7"]
-                 [menard "1.4.5"]]
+                 [menard "1.5.0"]]
   :global-vars {*warn-on-reflection* true}
   :main ^:skip-aot menard.lambda.def
   :uberjar-name "menard-lambda.jar"

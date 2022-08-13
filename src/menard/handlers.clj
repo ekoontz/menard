2f93536fa6b3025702b822435e84d45071decf51
Eugene Koontz <ekoontz@hiro-tan.org>
2022-08-13 16:51:18 +0200

    cleanup

--- src/menard/handlers.clj
+++ src/menard/handlers.clj
@@ -5,7 +5,6 @@
    [dag_unify.core :as u]
    [menard.english :as en]
    [menard.nederlands :as nl]
-   [menard.nederlands.woordenlijst :as woordenlijst]
    [menard.translate.spec :as tr]))
 
 (defn dag-to-string [dag]

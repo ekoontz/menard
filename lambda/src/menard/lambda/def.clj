d6d03fa0bf109cf8bd8e7f086612435c4db95138
Eugene Koontz <ekoontz@hiro-tan.org>
2022-09-11 00:53:26 +0200

    Revert "try to deploy only one lambda function"
    This reverts commit b527b93d374699550ae8fde1e417df8458ae826c.

--- lambda/src/menard/lambda/def.clj
+++ lambda/src/menard/lambda/def.clj
@@ -71,5 +71,115 @@
          (map str)
          json-response)))
 
+(h/deflambda AnalyzeNL
+  [event context]
+  (let [matching-lexemes
+        (-> event
+            :queryStringParameters
+            :q
+            ((fn [q] (handlers/analyze q "nl"))))]
+    (->> matching-lexemes
+         (map dag_unify.serialization/serialize)
+         (map str)
+         json-response)))
 
-(h/gen-main [#'AnalyzeEN])
+(h/deflambda Parse
+  [event context]
+  (-> event
+      :queryStringParameters
+      :q
+      handlers/parse-nl
+      json-response))
+
+(h/deflambda ParseNLStart
+  [event context]
+  (-> event
+      :queryStringParameters
+      :q
+      handlers/parse-nl-start
+      json-response))
+
+(h/deflambda ParseENStart
+  [event context]
+  (-> event
+      :queryStringParameters
+      :q
+      handlers/parse-en-start
+      json-response))
+
+(h/deflambda GenerateNL
+  [event context]
+  (let [spec (-> event :queryStringParameters :q)
+        model (-> event :queryStringParameters :model)
+        target-model (get-target-model model)
+        source-model (get-source-model model)]
+    (json-response (handlers/generate-nl-and-en-by-spec spec target-model source-model))))
+
+(h/deflambda GenerateEN
+  [event context]
+  (let [spec (-> event :queryStringParameters :spec)]
+    (json-response (handlers/generate-en spec))))
+
+(h/deflambda GenerateWithAlts
+  [event context]
+  (let [spec (-> event :queryStringParameters :spec)
+        alternates (-> event :queryStringParameters :alts)
+        model (-> event :queryStringParameters :model)
+        target-model (get-target-model model)
+        source-model (get-source-model model)]
+    (json-response (handlers/generate-nl-with-alternations spec alternates target-model source-model))))
+
+(h/deflambda GrammarEN
+  [event context]
+  (json-response (handlers/grammar "en")))
+
+(h/deflambda GrammarNL
+  [event context]
+  (json-response (handlers/grammar "nl")))
+
+(h/deflambda MorphologyEN
+  [event context]
+  (json-response (handlers/morphology "en")))
+
+(h/deflambda MorphologyNL
+  [event context]
+  (json-response (handlers/morphology "nl")))
+
+(h/deflambda RuleEN
+  [event context]
+  (let [matching-rules
+        (-> event
+            :queryStringParameters
+            :q
+            ((fn [q] (handlers/rules q "en"))))]
+    (->> matching-rules
+         (map dag_unify.serialization/serialize)
+         (map str)
+         json-response)))
+
+(h/deflambda RuleNL
+  [event context]
+  (let [matching-rules
+        (-> event
+            :queryStringParameters
+            :q
+            ((fn [q] (handlers/rules q "nl"))))]
+    (->> matching-rules
+         (map dag_unify.serialization/serialize)
+         (map str)
+         json-response)))
+
+;; TODO: deprecate and remove 'Generate in favor of 'GenerateNL.
+(h/gen-main [#'AnalyzeEN
+             #'AnalyzeNL
+             #'Parse
+             #'GenerateEN
+             #'GenerateNL
+             #'GenerateWithAlts
+             #'GrammarEN
+             #'GrammarNL
+             #'MorphologyEN
+             #'MorphologyNL
+             #'ParseNLStart
+             #'RuleEN
+             #'RuleNL])

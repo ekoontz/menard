c206079ddbffa97320da97a2d66e3a4b1db3fe4a
Eugene Koontz <ekoontz@hiro-tan.org>
2024-06-15 15:26:49 +0200

    upgrade to latest dag_unify release

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject menard "1.6.4"
+(defproject menard "1.7.0-SNAPSHOT"
   :description "A library for natural language generation and parsing"
   :url "http://github.com/ekoontz/menard"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[dag_unify "1.11.0"]
+  :dependencies [[dag_unify "1.11.1"]
                  [log4j/log4j "1.2.17"]
                  [org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "1.0.0"]

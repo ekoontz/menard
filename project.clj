8e0a908a0f09afc1437ac8de5498f69a7fe2be77
Eugene Koontz <ekoontz@hiro-tan.org>
2025-04-22 00:04:54 +0200

    add Spanish possessive pronouns

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject menard "1.9.2"
+(defproject menard "1.9.3-SNAPSHOT"
   :description "A library for natural language generation and parsing"
   :url "http://github.com/ekoontz/menard"
   :license {:name "Eclipse Public License"

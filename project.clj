344dc07c19fa8f46ae8fb9510f03a9d8c6ac9f4d
Eugene Koontz <ekoontz@hiro-tan.org>
2025-10-26 10:58:14 +0100

    upgrade some dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject menard "1.9.12"
+(defproject menard "1.9.13-SNAPSHOT"
   :description "A library for natural language generation and parsing"
   :url "http://github.com/ekoontz/menard"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[dag_unify "1.11.1"]
+  :dependencies [[dag_unify "1.11.2"]
                  [log4j/log4j "1.2.17"]
-                 [org.clojure/clojure "1.11.3"]
+                 [org.clojure/clojure "1.12.3"]
                  [org.clojure/data.json "1.0.0"]
-                 [org.clojure/tools.logging "1.2.4"]
+                 [org.clojure/tools.logging "1.3.0"]
                  [clojure.java-time "0.3.3"]
                  [org.clojure/core.async "1.5.648"]
                  [babashka/fs "0.1.2"]

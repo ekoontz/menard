54fd15699f2ee736c28111fa69d21f3bbd14f480
Eugene Koontz <ekoontz@hiro-tan.org>
2025-12-07 11:47:20 +0100

    release 1.9.19

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject menard "1.9.19-SNAPSHOT"
+(defproject menard "1.9.19"
   :description "A library for natural language generation and parsing"
   :url "http://github.com/ekoontz/menard"
   :license {:name "Eclipse Public License"
